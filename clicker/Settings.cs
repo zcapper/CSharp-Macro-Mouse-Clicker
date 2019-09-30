1c5d81a7bd18103a92636b1da96424d7e5dcf8d4
Cyril Fomenko <rik.pie@gmail.com>
2019-09-30 16:45:51 +0500

    Settings and autosave

---
+++ clicker/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clicker
+{
+    public class Settings
+    {
+        public bool Autorun { get; set; }
+        public string AutosaveFilename { get; set; }
+        public string SettingsFilename { get; set; }
+        public TimeSpan DefaultCooldown { get; set; }
+
+        public Settings()
+        {
+            AutosaveFilename = "autosave.json";
+            SettingsFilename = "settings.json";
+            DefaultCooldown = TimeSpan.FromSeconds(2);
+        }
+    }
+}
